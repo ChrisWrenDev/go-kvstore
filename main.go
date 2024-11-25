32febba1b3c5d4e059f628c01ccdb1106045d2df
Chris Wren <chriswrendeveloper@gmail.com>
2024-11-25 20:13:02 +0000

    KVStore constructor

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "sync"
+
 type Storer interface {
 	Put(string, []byte) error
 	Get(string )([]byte, error)
@@ -7,8 +9,24 @@ type Storer interface {
     Delete(string)([]byte, error)
 }
 
+type KVStore struct {
+    mu sync.RWMutex
+    data map[string][]byte
+}
 
+func NewKVStore() *KVStore {
+    return &KVStore{
+        data: make(map[string][]byte),
+
+    }
+}
+
+func putData(s Storer) error {
+    return s.Put("Name", []byte("tango terry"))
+}
 
 func main(){
+    kv := NewKVStore()
 
+    putData(kv)
 }
